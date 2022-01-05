12358d5c0a84fad208124bee6499d033f8e4c943
AN-GitHub-1 <89004665+AN-GitHub-1@users.noreply.github.com>
2022-01-05 14:40:02 +0900

    section 14 part 1

--- code/src/App.js
+++ code/src/App.js
@@ -1,31 +1,115 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // useCallback is used when we don't want to render a component every time and
+  // only render it conditionally.
+  // const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const url = "https://swapi.dev/api/films";
+      // const badUrl = "https://swapi.dev/api/films''''badbadbad";
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // http doesn't generate an error by default so we need to use this
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  // // .then() handles what happens after the information is fetched and .catch()
+  // // handles any potential errors.
+  // // .json transforms json into javascript code
+  // fetch(url)
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     // data is now the json object with key-value pairs
+  //     // the previous then() tranfromed it into js readable json
+  //     // remember that map returns a new array
+  //     const transformedMovies = data.results.map((movieData) => {
+  //       return {
+  //         id: movieData.episode_id,
+  //         title: movieData.title,
+  //         openingText: movieData.opening_crawl,
+  //         releaseDate: movieData.release_date,
+  //       };
+  //     });
+
+  //     setMovies(transformedMovies);
+  //   });
+  // };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length <= 0 && !error && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>} */}
+        {content}
       </section>
     </React.Fragment>
   );
