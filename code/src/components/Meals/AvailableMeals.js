3029b93a46e480a2e2577573b57f2827206d615b
AN-GitHub-1 <89004665+AN-GitHub-1@users.noreply.github.com>
2022-01-11 13:53:30 +0900

    html my version

--- code/src/components/Meals/AvailableMeals.js
+++ code/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,58 @@
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: 'm1',
+//     name: 'Sushi',
+//     description: 'Finest fish and veggies',
+//     price: 22.99,
+//   },
+//   {
+//     id: 'm2',
+//     name: 'Schnitzel',
+//     description: 'A german specialty!',
+//     price: 16.5,
+//   },
+//   {
+//     id: 'm3',
+//     name: 'Barbecue Burger',
+//     description: 'American, raw, meaty',
+//     price: 12.99,
+//   },
+//   {
+//     id: 'm4',
+//     name: 'Green Bowl',
+//     description: 'Healthy...and green...',
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  // useEffect can't directly use an async function
+  useEffect(() => {
+    fetch("https://react-http-b5f83-default-rtdb.firebaseio.com/meals.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const newMeals = [];
+
+        for (const meal in data) {
+          newMeals.push({
+            id: meal,
+            name: data[meal].name,
+            description: data[meal].description,
+            price: data[meal].price,
+          });
+        }
+        setMeals(newMeals);
+      });
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
