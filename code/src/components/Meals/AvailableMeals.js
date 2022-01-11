a7926d6dd44cdb7daa920bcf8219afd175dacb46
AN-GitHub-1 <89004665+AN-GitHub-1@users.noreply.github.com>
2022-01-11 14:28:52 +0900

    section 216

--- code/src/components/Meals/AvailableMeals.js
+++ code/src/components/Meals/AvailableMeals.js
@@ -31,25 +31,56 @@ import { useEffect, useState } from "react";
 // ];
 
 const AvailableMeals = () => {
+  // we need state because we need to refresh the component once the data loads
   const [meals, setMeals] = useState([]);
 
-  // useEffect can't directly use an async function
-  useEffect(() => {
-    fetch("https://react-http-b5f83-default-rtdb.firebaseio.com/meals.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const newMeals = [];
+  // My code
+  // useEffect(() => {
+  //   fetch("https://react-http-b5f83-default-rtdb.firebaseio.com/meals.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const newMeals = [];
 
-        for (const meal in data) {
-          newMeals.push({
-            id: meal,
-            name: data[meal].name,
-            description: data[meal].description,
-            price: data[meal].price,
-          });
-        }
-        setMeals(newMeals);
-      });
+  // // we need to do this specifically because of the Firebase structure
+  // // which stores as an object array,
+  // // map can only be used for arrays so we need to extract it with this
+  // // for in loop.
+  //       for (const key in data) {
+  //         newMeals.push({
+  //           id: key,
+  //           name: data[key].name,
+  //           description: data[key].description,
+  //           price: data[key].price,
+  //         });
+  //       }
+  //       setMeals(newMeals);
+  //     });
+  // }, []);
+
+  // useEffect can't directly use an async function
+  // make sure this runs only once with an empty dependency
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-b5f83-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
   }, []);
 
   const mealsList = meals.map((meal) => (
