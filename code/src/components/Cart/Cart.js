0876af5a0399a5a0306e3db03ecbaf9ad7f53235
AN-GitHub-1 <89004665+AN-GitHub-1@users.noreply.github.com>
2022-01-11 17:34:29 +0900

    end of 1/11

--- code/src/components/Cart/Cart.js
+++ code/src/components/Cart/Cart.js
@@ -1,12 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from "react";
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
-import classes from './Cart.module.css';
-import CartContext from '../../store/cart-context';
+import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -20,7 +22,7 @@ const Cart = (props) => {
   };
 
   const cartItems = (
-    <ul className={classes['cart-items']}>
+    <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
@@ -34,20 +36,35 @@ const Cart = (props) => {
     </ul>
   );
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-          Close
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
         </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
-    </Modal>
+      )}
+    </div>
+  );
+
+  return (
+    <>
+      <Modal onClose={props.onClose}>
+        {cartItems}
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>{totalAmount}</span>
+        </div>
+        {isCheckout && <Checkout onCancel={props.onClose} />}
+        {!isCheckout && modalActions}
+      </Modal>
+    </>
   );
 };
 
---
+++ code/src/components/Cart/Checkout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import classes from "./Checkout.module.css";
+
+const Checkout = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal"></input>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city"></input>
+      </div>
+      {/* Make sure to make this of type button so it doesn't submit the form */}
+      <button type="button" onClick={props.onCancel}>
+        Cancel
+      </button>
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default Checkout;
