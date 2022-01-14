ea41ac36813df361198447875493e3a689ad4b24
AN-GitHub-1 <89004665+AN-GitHub-1@users.noreply.github.com>
2022-01-14 16:38:15 +0900

    1/14 (unfinished)

--- code/src/components/Cart/Checkout.js
+++ code/src/components/Cart/Checkout.js
@@ -1,36 +1,88 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
-  const submitHandler = (event) => {
-    event.preventDefault();
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    postalCode: true,
+    city: true,
+  });
 
-    console.log(event.target);
+  const isEmpty = (value) => {
+    return value.trim() === "";
+  };
+  const isNotFiveChars = (value) => {
+    return value.trim().length !== 5;
   };
 
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = cityInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredPostalCodeIsValid = isNotFiveChars(enteredPostalCode);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+
+    setFormInputsValidity({
+      name: enteredNameIsValid,
+      street: enteredStreetIsValid,
+      postalCode: enteredPostalCodeIsValid,
+      city: enteredCityIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalCodeIsValid &&
+      enteredCityIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    // submit cart data
+  };
+
+  const invalidNameInput = "classes.invalid";
+
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name"></input>
+        <div className={classes.invalid}>
+          <label htmlFor="name">Your Name</label>
+          <input ref={nameInputRef} type="text" id="name" />
+        </div>
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street"></input>
+        <input ref={streetInputRef} type="text" id="street" />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal"></input>
+        <input ref={postalCodeInputRef} type="text" id="postal" />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city"></input>
+        <input ref={cityInputRef} type="text" id="city" />
       </div>
-      {/* Make sure to make this of type button so it doesn't submit the form */}
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit" className={classes.submit}>
+          Confirm
+        </button>
+      </div>
     </form>
   );
 };
